2318326921876ccbd497296883c9a9cab826e682
Projeto-IntegradorMEUI <121239199+Projeto-IntegradorMEUI@users.noreply.github.com>
2023-01-10 11:45:29 -0300

    Update Sobrenos.tsx

--- src/paginas/sobrenos/Sobrenos.tsx
+++ src/paginas/sobrenos/Sobrenos.tsx
@@ -18,11 +18,7 @@ function Sobrenos() {
   <Grid item xs={6} className='grid7'>
                     
                   <img src="https://cdn.discordapp.com/attachments/1014550882538504273/1052914685495738398/quem_somos-removebg-preview.png" alt="" width="550px" height="350px" />
-                  <img src="https://cdn.discordapp.com/attachments/1022862562569756762/1053022015457607810/eu-removebg-preview.png" alt="" width="200px" height="180px" />
-                  <img src="https://cdn.discordapp.com/attachments/1022862562569756762/1053019373218041957/20221206_124713-removebg-preview.png" alt="" width="200px" height="200px" />
-                  <img src="https://cdn.discordapp.com/attachments/1022862562569756762/1053019572267135047/IMG_20221215_152110-removebg-preview.png" alt="" width="230px" height="230px" />
-                  <img src="https://cdn.discordapp.com/attachments/1047476710578331680/1053266380876697610/IMG-20210917-WA0159-removebg-preview.png" alt="" width="330px" height="270px" />
-                  <img src="https://media.discordapp.net/attachments/1050176198895997051/1053328935431176332/FB_IMG_1670456969090-removebg-preview.png" alt="" width="220px" height="200px" />
+                
               
               </Grid>
                </Grid>
