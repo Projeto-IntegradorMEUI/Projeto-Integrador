01b7b532bd4a9b34de848ff8f99adb2273e9e7e2
Projeto-IntegradorMEUI <121239199+Projeto-IntegradorMEUI@users.noreply.github.com>
2023-01-10 10:23:12 -0300

    Update Home.tsx

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -35,7 +35,7 @@ function Home() {
   }, [token])
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa imgb' >
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa' >
                 <Grid alignItems="center" item xs={12}>
                     <Box paddingX={20} >
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo grid5'>Seja bem vindo ao site do Projeto MEUI</Typography>
