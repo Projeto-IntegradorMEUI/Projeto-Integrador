14714cc549ac1e842fa03684e807478ea7d9c88c
Projeto-IntegradorMEUI <121239199+Projeto-IntegradorMEUI@users.noreply.github.com>
2023-01-10 10:32:32 -0300

    Update Nossamissao.tsx

--- src/paginas/nossamissao/Nossamissao.tsx
+++ src/paginas/nossamissao/Nossamissao.tsx
@@ -6,7 +6,7 @@ import './Nossamissao.css';
 function Nossamissao() {
   return (
     <>
-    <Grid container justifyContent='center' alignItems='center' className='imgb'>
+    <Grid container justifyContent='center' alignItems='center'>
     <Grid alignItems="center" item xs={6}>
      <Box paddingX={0}>
  
